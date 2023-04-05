1c1754bdc5fbb176143707b33eaa0b69a552e1bb
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-04-05 01:55:10 -0300

    alterando footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,5 +3,5 @@ import './Footer.css'
 
 export default (props) => 
   <div className='Footer'>
-    <p>PROAD © 2017/2021 - Todos os direitos reservados</p>
+    <p>PROAD © 2017/2023 - Todos os direitos reservados</p>
   </div>
