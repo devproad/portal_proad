a96fba247a56d15a42bc1ef12f68585a1b002f9c
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-07-20 03:42:30 -0300

    feat: definindo limite de caracteres para noticias

--- src/components/DestaquesItem/DestaquesItem.jsx
+++ src/components/DestaquesItem/DestaquesItem.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import './DestaquesItem.css'
 
 const DestaquesItem = (props) => {
+    const titMaxCaracteres = 55;
+    const descMaxCaracteres = 150;
+
     return (
             <div className="card-destaque" id={props.id}>
                 <img src={props.imagem} alt="" className="image-card" />
-                <h1 className="title-card">{props.titulo}</h1>
-                <p className="description-card">{props.descricaoDestaques}</p>
+
+                {props.titulo.length > titMaxCaracteres ?
+                    <h1 className='title-card'>{props.titulo.substring(0, titMaxCaracteres)}...</h1>
+                    : <h1 className='title-card'>{props.titulo}</h1>
+                }
+                
+                {props.descricaoDestaques.length > descMaxCaracteres ?
+                    <p className='description-card'>{props.descricaoDestaques.substring(0, descMaxCaracteres)}...</p>
+                    : <p className='description-card'>{props.descricaoDestaques}</p>
+                }
                 <p className="data-card">{props.data}</p>
             </div>
   );
--- src/components/NoticiasRecentesItem/NoticiasRecentesItem.jsx
+++ src/components/NoticiasRecentesItem/NoticiasRecentesItem.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import './NoticiasRecentesItem.css'
 
 const NoticiasRecentes = (props) => {
+    const titMaxCaracteres = 55;
+    const descMaxCaracteres = 150;
     return (
         <div className='noticiasRecentesItem' id={props.id}>
-                <div className='itemNoticiasRecentes'>
-                    <div className='barraItemNoticias'>
-                       <span className='dataNoticiasRecentes'>{props.data}</span>
-                    </div>
-                    <div className='conteudoNoticiasRecentes'>
-                        <h1 className='tituloNoticiasRecentes'>{props.titulo}</h1>
-                        <p  className='DescricaoNoticiasRecentes'>{props.descricao}</p>
-                    </div>
+            <div className='itemNoticiasRecentes'>
+                <div className='barraItemNoticias'>
+                    <span className='dataNoticiasRecentes'>{props.data}</span>
                 </div>
+                <div className='conteudoNoticiasRecentes'>
+                 
+                    {props.titulo.length > titMaxCaracteres ? 
+                        <h1 className='tituloNoticiasRecentes'>{props.titulo.substring(0, titMaxCaracteres)}...</h1>
+                        : <h1 className='tituloNoticiasRecentes'>{props.titulo}</h1>
+                    }
+                 
+                    {props.descricao.length > descMaxCaracteres ?
+                            <p className='DescricaoNoticiasRecentes'>{props.descricao.substring(0, descMaxCaracteres)}...</p>
+                            : <p className='DescricaoNoticiasRecentes'>{props.descricao}</p>
+                    }
+
+                </div>
+            </div>
 
         </div>
     );
