43407b1c92a11a8659e3df488af5621f0804b389
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-07-28 04:50:08 -0300

    feat: implementando responsividade para noticias

--- src/pages/TodasNoticiasDcom.jsx
+++ src/pages/TodasNoticiasDcom.jsx
@@ -1,32 +1,58 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import LinksImportantes from '../components/LinksImportantes/LinksImportantes';
 import BarraSuperior from '../components/BarraSuperior/BarraSuperior'
 import Menu from '../components/Menu/Menu';
+import MenuMobile from '../components/Menu/MenuMobile';
 import MenuLateral from '../components/MenuLateral/MenuLateral';
 import SitesUteis from '../components/SitesUteis/sitesuteis';
 import TodasNoticias from '../components/Dcom/TodasNoticias/todasNoticias'
-import UltimosDocumentos from '../components/UltimosDocumentos/UltimosDocumentos';
-
 
 
 const TodasAsNoticias = () => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1000);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setIsDesktop(window.innerWidth > 1000);
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+
     return (
         <Fragment>
             <BarraSuperior></BarraSuperior>
-            <Menu></Menu>
+            {isDesktop ? <Menu /> : <MenuMobile />}
+            {isDesktop ? (
             <div className='Principal'>
                 <div className='esquerdo'>
                     <TodasNoticias></TodasNoticias>
                 </div>
                 <div className='direito'>
                     <MenuLateral>
-                        <UltimosDocumentos></UltimosDocumentos>
                         <LinksImportantes></LinksImportantes>
                         <SitesUteis></SitesUteis>
                     </MenuLateral>
-
                 </div>
             </div>
+                ) : (
+                    <div className='Principal'>
+                        <div className='esquerdo'>
+                        <MenuLateral>
+                        <LinksImportantes></LinksImportantes>
+                        <SitesUteis></SitesUteis>
+                    </MenuLateral>
+                        </div>
+                        <div className='direito'>
+                            <TodasNoticias></TodasNoticias>
+                        </div>
+                    </div>)}
+
         </Fragment>
     );
 }
