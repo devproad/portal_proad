2a4c53416b2b25885353fcc5eff8b0c0b8f71cd2
Rúben Filipe <rubenfilipe.net@gmail.com>
2023-08-21 15:03:15 -0300

    feat: implementando google g-tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import TodasNoticias from './pages/TodasNoticias';
 import TodasNoticiasDcom from './pages/TodasNoticiasDcom';
 import Dcom from './pages/Dcom';
+import ReactGA from 'react-ga';
 
+ const App = () => (
+  useEffect(() => {
+    ReactGA.initialize('G-F42VZ617CC');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []),
 
- const App = (props) => (
   <div className='App'>
      <BrowserRouter>
       <Routes>
